6dd488f45809ea0f9adb2c96b83300a8291f422d
Julienraptor01 <julienraptor01@gmail.com>
2022-03-06 21:47:39 +0100

    Update CimtbWailaPlugin.java

--- src/main/java/me/shedaniel/cimtb/CimtbWailaPlugin.java
+++ src/main/java/me/shedaniel/cimtb/CimtbWailaPlugin.java
@@ -50,7 +50,7 @@ public class CimtbWailaPlugin implements IWailaPlugin, IBlockComponentProvider {
             tooltipTexts.add(new TranslatableText("cimtb.harvestable").formatted(Formatting.GRAY).append(new TranslatableText("cimtb.harvestable.symbol." + harvestable)));
             tooltipTexts.add(new TranslatableText("cimtb.effective_tool").formatted(Formatting.GRAY).append(handler.getToolDisplay()));
 
-            if (level == 0 && handler.toolType() == "pickaxes" || level > 0) {
+            if (level >= 0 && handler.toolType() == "pickaxes") {
                 int[] textColor = {11184810};
                 String text = level + "";
                 if (I18n.hasTranslation("cimtb.harvest_level.level." + text)) {
